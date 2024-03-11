2568d414585247244c182bc5889929ec1ea84eda
yungd1plomat <aynur.musin.06@mail.ru>
2024-03-11 22:40:47 +0500

    add graph

--- bot.py
+++ bot.py
@@ -7,10 +7,21 @@ import coloredlogs, logging
 import discord
 from discord.ext import tasks
 from queue import Queue
+import matplotlib.pyplot as plt
+import mplcyberpunk
+import pandas as pd
+from datetime import datetime, timedelta
+import matplotlib.dates as md
+import numpy as np
+import io
+
+plt.style.use('cyberpunk')
 
 coloredlogs.install(level='DEBUG', fmt='%(asctime)s [%(levelname)s] %(message)s')
-logger = logging.getLogger('discord')
-logger.setLevel(logging.INFO)
+logging.disable(logging.CRITICAL)
+logging.getLogger('proxyclient').setLevel(logging.DEBUG)
+logging.getLogger('discord').setLevel(logging.INFO)
+logging.getLogger('discord.ext').setLevel(logging.INFO)
 
 LOGIN = "rtanks_bot"
 PASSWORD="rtanks_bot123"
@@ -68,29 +79,33 @@ def parse_online():
         client.disconnect() 
         sleep(300)
 
-def get_last_online():
-    cursor.execute('''
-        SELECT online
-        FROM online
-        ORDER BY date DESC
-        LIMIT 1
-    ''')
-    last_online = cursor.fetchone()[0]
-    return last_online
+def plot_online_data(hours_ago = 24, time_freq = '1h'):
+    cur = db.cursor()
+    now = datetime.now()
+    date = now - timedelta(hours=hours_ago)
+    cur.execute("SELECT date, online FROM online WHERE date >= ? ORDER BY date", (date,))
+    rows = cur.fetchall()
+    df = pd.DataFrame(rows, columns=['date', 'online'])
+    df['date'] = pd.to_datetime(df['date'])
+    min_time = df['date'].min().round(freq=time_freq)
+    max_time = df['date'].max().round(freq=time_freq)
+    plt.plot(df['date'], df['online'])
+    plt.xlabel('Time (MSK)')
+    plt.ylabel('Online')
 
-def get_average_online_last_n_hours(n):
-    current_time = datetime.now()
-    start_time = current_time - timedelta(hours=n)
-    start_time_str = start_time.strftime('%Y-%m-%d %H:%M:%S')
-    current_time_str = current_time.strftime('%Y-%m-%d %H:%M:%S')
-    query = '''
-        SELECT AVG(online)
-        FROM online
-        WHERE date >= ? AND date <= ?
-    '''
-    cursor.execute(query, (start_time_str, current_time_str))
-    average_online = cursor.fetchone()[0]
-    return int(average_online)
+    plt.yticks(np.arange(0, max(df['online']) + 10, step=10))
+    plt.gca().xaxis.set_major_formatter(md.DateFormatter('%H:%M'))
+
+    time_range = pd.date_range(min_time, max_time, freq=time_freq)
+    plt.xticks(time_range, rotation=45)
+    for time in md.date2num(time_range):
+        online = round(np.interp(time, md.date2num(df['date']), df['online'].values))
+        plt.text(time, online, str(online), ha='center', va='center', fontsize=7, color='white', bbox=dict(facecolor='black', alpha=0.7, boxstyle='round'))
+    mplcyberpunk.add_glow_effects()
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    return buf
 
 @tasks.loop(seconds=5)
 async def update_presence():
@@ -109,12 +124,18 @@ async def on_ready():
     await update_presence.start()
 
 @bot.command(description="Get Rtanks online")
-async def online(ctx: discord.ApplicationContext):
-    last_online = get_last_online()
-    last_hour = get_average_online_last_n_hours(1)
-    last_half_day = get_average_online_last_n_hours(12)
-    last_day = get_average_online_last_n_hours(24)
-    await ctx.respond(f"Current online: {last_online}\nLast hour: {last_hour}\nLast 12 hours: {last_half_day}\nLast 24 hours: {last_day}")
+async def online(ctx: discord.ApplicationContext,
+                 time: discord.Option(str, choices=['halfhour', 'hour', 'halfday', 'day'])):
+    if time == "halfhour":
+        plot = plot_online_data(0.5, '5min')
+    elif time == "hour":
+        plot = plot_online_data(1, '15min')
+    elif time == 'halfday':
+        plot = plot_online_data(12, '1h')
+    else:
+        plot = plot_online_data(24, '1h')
+    await ctx.send(content=f'Online for {time}', file=discord.File(plot, filename='image.png'))
+
 
 bot.run("MTIxNjMzODAwMjIxODA1NzgyOQ.GfnmLP.v0GRYzSp9K8KNbfRcZswVZiXRJG_csrIs6QDmU")
 
